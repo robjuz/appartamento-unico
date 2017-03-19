e9fb53d70d2b6028a527f24a60e1768a87fc108e
robjuz <robjuz@gmail.com>
2017-03-19 19:26:54 +0100

    modified:   css/css.css 	modified:   index.html 	modified:   js/js.js

--- js/js.js
+++ js/js.js
@@ -2,46 +2,12 @@
  * Created by robjuz on 17/03/2017.
  */
 "use strict";
-var viewHeight;
-
-$(document).ready(function() {
-    $('.box').removeClass('fixed');
-    $('.box').css('position', 'static');
-    $('.box').each(function () {
-        $(this).css('top', $(this).offset().top);
-        $(this).attr('data-top', $(this).offset().top);
-    });
-    $('.box').css('position', 'absolute');
-    viewHeight = $('body').height();
-});
-
-$(window).scroll(function () {
-    var scrollTop = $(document).scrollTop();
-    $('.box:not(.last):not(.fixed)').each(function () {
-        var offsetTop = $(this).offset().top;
-        var height = $(this).find('.content').height();
-        var breakpoint = offsetTop + height - viewHeight + 50;
-
-        if((breakpoint <= scrollTop)) {
-            $(this).css('top', 'unset');
-            $(this).css('bottom', 0);
-            $(this).addClass('fixed');
-        }
-    });
-    $('.box').each(function () {
-        var offsetTop = $(this).data('top');
-        var height = $(this).find('.content').height();
-        var breakpoint = offsetTop + height - viewHeight + 50;
-        if ( breakpoint >= scrollTop ) {
-            $(this).removeClass('fixed');
-            $(this).css('top', $(this).data('top'));
-            $(this).css('bottom', 'unset');
-        }
-    });
-
-});
-
-$('#menu').find('a').click(function(e) {
+$('#menu, #logo').find('a').click(function(e) {
     e.preventDefault();
-    $('html, body').animate({scrollTop : $($(this).attr('href')).data('top')}, 'slow');
+    $('html, body').animate({scrollTop : $($(this).attr('href')).offset().top - $('#menu').height() + 1}, 'slow');
+});
+
+$('#menu').find('a').click(function (e){
+   $('#menu').find('a').removeClass('active');
+   $(this).addClass('active');
 });
