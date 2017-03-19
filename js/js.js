198b9ca7ee7781b862340b6b2d4dbedb1f7155c2
robjuz <robjuz@gmail.com>
2017-03-19 20:00:12 +0100

    modified:   css/css.css 	modified:   index.html 	modified:   js/js.js

--- js/js.js
+++ js/js.js
@@ -2,12 +2,43 @@
  * Created by robjuz on 17/03/2017.
  */
 "use strict";
-$('#menu, #logo').find('a').click(function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop : $($(this).attr('href')).offset().top - $('#menu').height() + 1}, 'slow');
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        var $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - $('#menu').height() +2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
 });
 
-$('#menu').find('a').click(function (e){
-   $('#menu').find('a').removeClass('active');
-   $(this).addClass('active');
-});
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#menu a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > (scrollPos - $('#menu').height()) ) {
+            $('#menu a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
