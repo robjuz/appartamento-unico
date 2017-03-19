7e790bb59aaa9a36eb3abdbe1243ebd45e412926
robjuz <robjuz@gmail.com>
2017-03-19 13:19:26 +0100

    v0.1 	modified:   css/css.css 	modified:   index.html 	modified:   js/js.js

--- js/js.js
+++ js/js.js
@@ -2,45 +2,9 @@
  * Created by robjuz on 17/03/2017.
  */
 "use strict";
+var viewHeight;
 
 $(document).ready(function() {
-    menu();
-    offset();
-});
-$(window).resize(menu);
-$(window).scroll(function () {
-    var top = $(document).scrollTop();
-    $('.box:not(.last):not(.fixed)').each(function () {
-        if(($(this).offset().top <= top)) {
-            $(this).css('top', 0);
-            $(this).addClass('fixed');
-        }
-    });
-    $('.box').each(function () {
-        if ( $(this).data('top') > top ) {
-            $(this).removeClass('fixed')
-            $(this).css('top', $(this).data('top'));
-        }
-    });
-
-});
-
-$('#menu').find('a').click(function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    $('html, body').animate({scrollTop : $($this.attr('href')).data('top')}, 'slow');
-});
-
-function menu() {
-    var menu = $('#menu');
-    if ($(window).height() < menu.height()) {
-        // menu.removeClass('fixed');
-    } else {
-        menu.addClass('fixed');
-    }
-}
-
-function offset() {
     $('.box').removeClass('fixed');
     $('.box').css('position', 'static');
     $('.box').each(function () {
@@ -48,4 +12,36 @@ function offset() {
         $(this).attr('data-top', $(this).offset().top);
     });
     $('.box').css('position', 'absolute');
-}
+    viewHeight = $('body').height();
+});
+
+$(window).scroll(function () {
+    var scrollTop = $(document).scrollTop();
+    $('.box:not(.last):not(.fixed)').each(function () {
+        var offsetTop = $(this).offset().top;
+        var height = $(this).find('.content').height();
+        var breakpoint = offsetTop + height - viewHeight + 50;
+
+        if((breakpoint <= scrollTop)) {
+            $(this).css('top', 'unset');
+            $(this).css('bottom', 0);
+            $(this).addClass('fixed');
+        }
+    });
+    $('.box').each(function () {
+        var offsetTop = $(this).data('top');
+        var height = $(this).find('.content').height();
+        var breakpoint = offsetTop + height - viewHeight + 50;
+        if ( breakpoint >= scrollTop ) {
+            $(this).removeClass('fixed');
+            $(this).css('top', $(this).data('top'));
+            $(this).css('bottom', 'unset');
+        }
+    });
+
+});
+
+$('#menu').find('a').click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop : $($(this).attr('href')).data('top')}, 'slow');
+});
